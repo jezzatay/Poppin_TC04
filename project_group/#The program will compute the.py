9c08ffea4642d819b9556c26e425e7da8cb3c00f
tanmingzhi <123954302+mingzhitan@users.noreply.github.com>
2023-01-31 00:47:15 +0800

    Update #The program will compute the.py

--- project_group/#The program will compute the.py
+++ project_group/#The program will compute the.py
@@ -1,4 +1,4 @@
-#The program will compute the vgvgvgvhjvh
+#The program will compute the
 #difference in the net profit column if net profit on
 #the current day is lower than the previous day
 from pathlib import Path
