4f43f089e666b99eb16ed550d2d200e5ad5f8295
tanmingzhi <123954302+mingzhitan@users.noreply.github.com>
2023-01-31 12:36:48 +0800

    Update profit_and_loss.py

--- project_group/profit_and_loss.py
+++ project_group/profit_and_loss.py
@@ -1,4 +1,4 @@
-#The program will compute the
+#The program will compute theheiguiegiuoe
 #difference in the net profit column if net profit on
 #the current day is lower than the previous day
 from pathlib import Path
